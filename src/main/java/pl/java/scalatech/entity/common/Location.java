559005db03359ea207915bd6bbb221cf7aabbd8a
Slawomir Borowiec <przodownik@tlen.pl>
2014-12-15 13:40:39 +0100

    @Builder , @NoArg , @AllArg constructor to Location entity added

--- src/main/java/pl/java/scalatech/entity/common/Location.java
+++ src/main/java/pl/java/scalatech/entity/common/Location.java
@@ -1,20 +1,28 @@
 package pl.java.scalatech.entity.common;
 
+
+
 import java.io.Serializable;
 
 import javax.persistence.Embeddable;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Builder;
 
 
 
 @Embeddable
 @XmlRootElement(name = "address")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Location implements Serializable {  
     private static final long serialVersionUID = -2191631126853132555L;
-    private final Double latitude;
-    private final Double longitude;
+    private  Double latitude;
+    private  Double longitude;
 
 }
