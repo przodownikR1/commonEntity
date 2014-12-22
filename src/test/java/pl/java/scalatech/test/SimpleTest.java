ef7f22eadd3deba2bc9d83f4f159ef97889ba8c2
Slawomir Borowiec <przodownik@tlen.pl>
2014-12-22 14:59:48 +0100

    simple test

---
+++ src/test/java/pl/java/scalatech/test/SimpleTest.java
@@ -0,0 +1,12 @@
+package pl.java.scalatech.test;
+
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+
+public class SimpleTest {
+    
+    @Test
+    public void shouldWork() {
+        Assertions.assertThat(true);
+    }
+}
